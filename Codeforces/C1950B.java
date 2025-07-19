4f12f3aee6b1e56b42dfbd82d1d59d125f789477
Sameer <sameer2akhmal@gmail.com>
2025-07-19 11:59:28 +0530

    Added Leetcode

--- Codeforces/C1950B.java
+++ Codeforces/C1950B.java
@@ -15,17 +15,19 @@ public class C1950B {
         int t = scan.nextInt();
         while (t-- > 0) {
             int n = scan.nextInt();
-            for (int i = 1; i <= n*2 ; i++) {
-                for (int j = 1; j <= n*2; j++) {
-                    if(i == j || i + 1 == j || i == j + 1){
-                        System.out.print("#");
-                    }
-                    else{
-                        System.out.print(".");
-                    }
-                    System.out.println();
-                }
+            
+            for (int i = 0; i < n; i++) {
+                StringBuilder s = new StringBuilder();
                 
+                for (int j = 0; j < n; j++) {
+                    if ((i + j) % 2 == 0) {
+                        s.append("##");
+                    } else {
+                        s.append("..");
+                    }
+                }
+                System.out.println(s);
+                System.out.println(s);
             }
         }
         
